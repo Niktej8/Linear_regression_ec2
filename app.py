ad61b7ecbbe5ad6f65266e532f12191b345d2ce7
Niktej8 <97293189+Niktej8@users.noreply.github.com>
2025-11-13 06:11:30 +0530

    changed the host and port

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def predict():
         return render_template("index.html", error="Please enter a valid number.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
